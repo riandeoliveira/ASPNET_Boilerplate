e2e19153910fe76767dcf1c033eeec10b6c2bbda
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-26 19:05:48 -0300

    feat: create User model

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,17 +6,12 @@ namespace ASPNET_Boilerplate.Controllers;
 [Route("/api/users")]
 public class UserController : ControllerBase {
   [HttpGet]
+  [Route("/api/users/all")]
   public IActionResult Get() {
     object person = new {
       name = "John Doe",
       age = 45
     };
-
-    int i = 0;
-    string name = "John";
-
-    Console.WriteLine(i);
-    Console.WriteLine(name);
 
     return Ok(person);
   }
