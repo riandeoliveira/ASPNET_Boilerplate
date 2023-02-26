9b049ea1fc96c0c2651dc49cb4a2e6a3549decfe
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-26 20:13:00 -0300

    feat: improve User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPNET_Boilerplate.Models;
 
 public class User {
   public int Id { get; set; }
+
+  [Required]
+  [EmailAddress]
   public string? Email { get; set; }
+
+  [Required]
   public string? Name { get; set; }
+
+  [Required]
   public string? Password { get; set; }
+
+  [Required]
   public string? Username { get; set; }
 }
