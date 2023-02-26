c07c024bc9abd3a7edd5bba0e34fbfa13b8e2576
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-26 20:13:09 -0300

    feat: improve UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,18 +1,35 @@
+using ASPNET_Boilerplate.Database;
+using ASPNET_Boilerplate.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPNET_Boilerplate.Controllers;
 
 [ApiController]
 [Route("/api/users")]
 public class UserController : ControllerBase {
+  public readonly DataContext _context;
+
+  public UserController(DataContext context) {
+    _context = context;
+  }
+
   [HttpGet]
   [Route("/api/users/all")]
-  public IActionResult Get() {
-    object person = new {
-      name = "John Doe",
-      age = 45
-    };
+  public async Task<ActionResult<List<User>>> GetAllUsers() {
+    List<User> users = await _context.Users.ToListAsync();
 
-    return Ok(person);
+    return Ok(users);
+  }
+
+  [HttpPost]
+  [Route("/api/users/register")]
+  public async Task<ActionResult<List<User>>> Register(User user) {
+    _context.Users.Add(user);
+
+    await _context.SaveChangesAsync();
+    List<User> users = await _context.Users.ToListAsync();
+
+    return Ok(users);
   }
 }
